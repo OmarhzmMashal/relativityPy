9b069b6ef47324ea215a3133ab8736b51619ee47
Omar Hazem Mashaal <o.h.mashal@gmail.com>
2021-08-31 23:42:32 +0200

    fix

--- relativityPy.py
+++ relativityPy.py
@@ -1,7 +1,7 @@
 from sympy import *
 import numpy as np
 
-class ReimannManifold:
+class RiemannManifold:
     
     # set hyperspace and its variables 
     
@@ -13,6 +13,7 @@ class ReimannManifold:
     
     
     def pGammaString(self,u,v,i,val):
+        
         gamma = "Γ {}".format(u)
         gamma = gamma.translate(str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹"))
         gamma = gamma + "{}{}".format(v,i)
@@ -21,8 +22,8 @@ class ReimannManifold:
 
     
     def getChristofellSymbols(self, string=False):
-        # N x N x N 
-        christofell_symbols = [[[0] * self.d for i in range(self.d)] for j in range(self.d)]
+        
+        christofell_symbols = [[[0] * self.d for i in range(self.d)] for j in range(self.d)]        
         
         for i in range(self.d):
             for j in range(self.d):
@@ -35,7 +36,6 @@ class ReimannManifold:
                                                                                - diff(self.metric[j][k],self.coor[d]))
 
                     gamma = simplify(gamma)
-        
                     christofell_symbols[i][j][k] = gamma
                     
                     if string == True and gamma != 0:
@@ -45,6 +45,7 @@ class ReimannManifold:
 
     
     def pRiemannTensorString(self,p,i,j,k,val):
+        
         R = "R {}".format(p)
         R = R.translate(str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹"))
         R = R + "{}{}{}".format(i,j,k)
@@ -54,13 +55,11 @@ class ReimannManifold:
         
     def getRiemannTensor(self, string=False):
         
-        # N x N x N x N
         tensor_components = [[[[0] * self.d for i in range(self.d)] for j in range(self.d)] for k in range(self.d)]
         
         cs = self.getChristofellSymbols()
 
-        for p in range(self.d):
-           
+        for p in range(self.d):   
             for i in range(self.d):
                 for j in range(self.d):
                     for k in range(self.d):
@@ -68,16 +67,15 @@ class ReimannManifold:
                         part1 = diff(cs[p][k][i], self.coor[j]) - diff(cs[p][j][i], self.coor[k])
                         
                         # summing over dummy a
-                        s1 = 0
+                        summation1 = 0
                         for a in range(self.d):
-                            s1+=cs[a][k][i] * cs[p][j][a]
+                            summation1 += cs[a][k][i] * cs[p][j][a]
                         
-                        s2=0
+                        summation2 = 0
                         for a in range(self.d):
-                            s2+=cs[a][j][i] * cs[p][k][a]
+                            summation2 += cs[a][j][i] * cs[p][k][a]
                                                 
-                        component = simplify(part1 + s1 - s2)
-                        
+                        component = simplify(part1 + summation1 - summation2)
                         tensor_components[p][i][j][k] = component 
                     
                         if string == True and component != 0:
@@ -87,6 +85,7 @@ class ReimannManifold:
 
 
     def pRicciTensorString(self, i,j, val):
+        
         R = "R{}{}".format(i,j,k)
         R = R.translate(str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉"))
         print(R + " = {}".format(val))
@@ -94,7 +93,6 @@ class ReimannManifold:
         
     def getRicciTensor(self, string=False):
         
-        # N x N 
         tensor_components = [[0] * self.d for i in range(self.d)]
         
         rt = self.getRiemannTensor(string=True)
@@ -102,129 +100,40 @@ class ReimannManifold:
         for i in range(self.d):
             for j in range(self.d):
 
-                    # summing over dummy a
-                    s = 0
+                    # summing over dummy p
+                    summation = 0
                     for p in range(self.d):
-                        s += rt[p][i][p][j]
+                        summation += rt[p][i][p][j]
 
-                    s = simplify(s)
-                    tensor_components[i][j] = s
+                    summation = simplify(summation)
+                    tensor_components[i][j] = summation
 
-                    if string == True and s != 0:
-                        self.pRicciTensorString(i,j,s)
+                    if string == True and summation != 0:
+                        self.pRicciTensorString(i,j,summation)
                             
         return tensor_components
     
-    
-    def getLowerIndecies(self, tensor, n):
-        
-        # N x N 
-        tensor_components = [[0] * self.d for i in range(self.d)]
-        
-        rt = self.getRiemannTensor(string=True)
 
-        for i in range(self.d):
-            for j in range(self.d):
-
-                    # summing over dummy a
-                    s = 0
-                    for p in range(self.d):
-                        s += rt[p][i][p][j]
-
-                    s = simplify(s)
-                    tensor_components[i][j] = s
-
-                    if string == True and s != 0:
-                        self.pRicciTensorString(i,j,s)
-                            
-        return tensor_components
-
-
-    
-    def vis():
-        
-        rspace = 10.0 #space
-        
-        anglespace = 2*pi
-        
-        nx = 1000 #discretize
-        
-        dx = xspace/nx #increment
-        dtheta = anglespace/nx
-        dphi = anglespace/nx
-        
-        r = 2
-        
-        reimann_tensor = rm.getRiemannTensor(string=True)
-        
-        r_vals = np.linspace(0,rspace,nx) 
-        theta_vals = np.linspace(0,anglespace,nx) 
-        phi_vals = np.linspace(0,anglespace,nx) 
-        
-        
-        x = r * sin(theta) * cos(phi)
-        y = r * sin(theta) * sin(phi)
-        z = r * cos(theta)
-        
-        
-        lam_x = lambdify([r, phi, theta], x, modules=['numpy'])
-        lam_y = lambdify([r, phi, theta], x, modules=['numpy'])
-        lam_z = lambdify([r, phi, theta], x, modules=['numpy'])
-        
-        print(lam_x, lam_y , lam_z)
-        
-        
-
-
-# swchizler
+# Schwarzschild 
 #M, t, theta, r, phi= symbols(" M t theta r phi")
 #metric = [[-(1-2*M/r),0,0,0], [0,1/(1-2*M/r),0,0], [0,0,r**2,0], [0,0,0,r**2*sin(theta)**2]]
 #coor = [t,r,theta,phi]
 
 
-# rindler
+# Rindler
 #x, t, D = symbols(" x t D")
 #metric = [[(x/D)**2, 0] , [0, -1]]
 #coor = [t, x]
 
 
-# spher
+# Sphere
 r, theta, phi = symbols(" r theta phi")
 metric = [[1, 0, 0] , [0, r**2 , -1], [0,0, r**2*sin(theta)**2]]
 coor = [r, phi, theta]
 
 
-# spher
-#r, theta = symbols(" r theta")
-#metric = [[r**2 , 0], [0, r**2*sin(theta)**2]]
-#coor = [r, theta]
+rm = RiemannManifold(coor, metric)
+gammas = rm.getChristofellSymbols(string=True)
+riemann_tensor = rm.getRiemannTensor(string=True)
+ricci_tensor = rm.getRicciTensor(string=True)
 
-
-rm = ReimannManifold(coor, metric)
-#gammas = rm.getChristofellSymbols(string=True)
-#ricci_tensor = rm.getRicciTensor(string=True)
-reimann_tensor = rm.getRiemannTensor(string=True)
-
-
-e0=0
-e1=0
-e2=0
-n=len(coor)
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            e0 += reimann_tensor[0][i][j][k]
-
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            e1 += reimann_tensor[1][i][j][k]
-
-            
-e0 = simplify(e0)
-e1 = simplify(e1)
-e2 = simplify(e2)
-
-print(e0)
-print(e1)
-print(e2)
