9e8764ad1767e7d0c3532dd4be92b4a0b31ad69b
Omar Hazem Mashaal <o.h.mashal@gmail.com>
2021-09-01 08:16:01 +0200

    update

--- relativityPy.py
+++ relativityPy.py
@@ -1,10 +1,9 @@
 from sympy import *
 import numpy as np
+import math 
 
-class RiemannManifold:
-    
-    # set hyperspace and its variables 
-    
+class RiemannManifold: 
+   
     def __init__(self, coor, metric):
     
         self.coor = coor
@@ -21,7 +20,7 @@ class RiemannManifold:
         print(gamma + " = {}".format(val))
 
     
-    def getChristofellSymbols(self, string=False):
+    def getChristoffelSymbols(self, printstr=False):
         
         christofell_symbols = [[[0] * self.d for i in range(self.d)] for j in range(self.d)]        
         
@@ -38,7 +37,7 @@ class RiemannManifold:
                     gamma = simplify(gamma)
                     christofell_symbols[i][j][k] = gamma
                     
-                    if string == True and gamma != 0:
+                    if printstr == True and gamma != 0:
                         self.pGammaString(i,j,k,gamma)               
         
         return christofell_symbols
@@ -53,11 +52,11 @@ class RiemannManifold:
         print(R + " = {}".format(val))
 
         
-    def getRiemannTensor(self, string=False):
+    def getRiemannTensor(self, printstr=False):
         
         tensor_components = [[[[0] * self.d for i in range(self.d)] for j in range(self.d)] for k in range(self.d)]
         
-        cs = self.getChristofellSymbols()
+        cs = self.getChristoffelSymbols()
 
         for p in range(self.d):   
             for i in range(self.d):
@@ -78,7 +77,7 @@ class RiemannManifold:
                         component = simplify(part1 + summation1 - summation2)
                         tensor_components[p][i][j][k] = component 
                     
-                        if string == True and component != 0:
+                        if printstr == True and component != 0:
                             self.pRiemannTensorString(p,i,j,k,component)
                             
         return tensor_components
@@ -91,49 +90,159 @@ class RiemannManifold:
         print(R + " = {}".format(val))
         
         
-    def getRicciTensor(self, string=False):
+    def getRicciTensor(self, printstr=False):
         
         tensor_components = [[0] * self.d for i in range(self.d)]
         
-        rt = self.getRiemannTensor(string=True)
+        rt = self.getRiemannTensor()
 
         for i in range(self.d):
             for j in range(self.d):
 
-                    # summing over dummy p
-                    summation = 0
-                    for p in range(self.d):
-                        summation += rt[p][i][p][j]
+                # summing over dummy p
+                summation = 0
+                for p in range(self.d):
+                    summation += rt[p][i][p][j]
 
-                    summation = simplify(summation)
-                    tensor_components[i][j] = summation
+                summation = simplify(summation)
+                tensor_components[i][j] = summation
 
-                    if string == True and summation != 0:
-                        self.pRicciTensorString(i,j,summation)
+                if printstr == True and summation != 0:
+                    self.pRicciTensorString(i,j,summation)
                             
         return tensor_components
     
+    
+class RiemannManifoldSolution:
+    
+    def __init__(self):
+        pass
+    
+    
+    def schwarzschild(self, returned_tensor="riemann", printstr=False):
 
-# Schwarzschild 
-#M, t, theta, r, phi= symbols(" M t theta r phi")
-#metric = [[-(1-2*M/r),0,0,0], [0,1/(1-2*M/r),0,0], [0,0,r**2,0], [0,0,0,r**2*sin(theta)**2]]
-#coor = [t,r,theta,phi]
+        M, t, theta, r, phi= symbols(" M t theta r phi")
+        metric = [[-(1-2*M/r),0,0,0], [0,1/(1-2*M/r),0,0], [0,0,r**2,0], [0,0,0,r**2*sin(theta)**2]]
+        coor = [t,r,theta,phi]
+        
+        rm = RiemannManifold(coor, metric)
+
+        if returned_tensor == "riemann":
+            return rm.getRiemannTensor(printstr)
+        
+        elif returned_tensor == "gammas":
+            return  rm.getChristoffelSymbols(printstr)
+            
+        elif returned_tensor == "ricci":
+            return rm.getRicciTensor(printstr)
 
 
-# Rindler
-#x, t, D = symbols(" x t D")
-#metric = [[(x/D)**2, 0] , [0, -1]]
-#coor = [t, x]
+    def rindler(self, returned_tensor="riemann", printstr=False):
+
+        # where D = speed of light squared / proper acceleration
+        x, t, D = symbols(" x t D")
+        metric = [[(x/D)**2, 0] , [0, -1]]
+        coor = [t, x]
+        
+        rm = RiemannManifold(coor, metric)
+
+        if returned_tensor == "riemann":
+            return rm.getRiemannTensor(printstr)
+        
+        elif returned_tensor == "gammas":
+            return  rm.getChristoffelSymbols(printstr)
+            
+        elif returned_tensor == "ricci":
+            return rm.getRicciTensor(printstr)
+        
+    
+    def sphere(self, returned_tensor="riemann", printstr=False):
+
+        # where D = speed of light squared / proper acceleration
+        r, theta, phi = symbols(" r theta phi")
+        metric = [[1, 0, 0] , [0, r**2 , -1], [0,0, r**2*sin(theta)**2]]
+        coor = [r, phi, theta]
+        
+        rm = RiemannManifold(coor, metric)
+
+        if returned_tensor == "riemann":
+            return rm.getRiemannTensor(printstr)
+        
+        elif returned_tensor == "gammas":
+            return  rm.getChristoffelSymbols(printstr)
+            
+        elif returned_tensor == "ricci":
+            return rm.getRicciTensor(printstr)
+        
+
+    def euclidean(self, returned_tensor="riemann", printstr=False):
+
+        # where D = speed of light squared / proper acceleration
+        x, y, z = symbols(" x y z")
+        metric = [[1, 0, 0] , [0, 1 , 0], [0,0, 1]]
+        coor = [x, y, z]
+        
+        rm = RiemannManifold(coor, metric)
+
+        if returned_tensor == "riemann":
+            return rm.getRiemannTensor(printstr)
+        
+        elif returned_tensor == "gammas":
+            return  rm.getChristoffelSymbols(printstr)
+            
+        elif returned_tensor == "ricci":
+            return rm.getRicciTensor(printstr)    
 
 
-# Sphere
-r, theta, phi = symbols(" r theta phi")
-metric = [[1, 0, 0] , [0, r**2 , -1], [0,0, r**2*sin(theta)**2]]
-coor = [r, phi, theta]
+class CoordinateTransformation:
+    
+    def __init__(self, velocity):
+        
+        self.v1 = velocity[1]
+        self.v2 = velocity[2]
+        self.v3 = velocity[3]
+    
+    def lorentzT(self, vector):
+        
+        c = 299792458
+        V = math.sqrt(self.v1**2 + self.v2**2 + self.v3**2)
+        lf = 1/math.sqrt(1-(V**2/c**2))
+        
+        B1 = self.v1/c
+        B2 = self.v2/c
+        B3 = self.v3/c
+                
+        lB = [[lf,-lf*self.v1,-lf*self.v2,-lf*self.v3],
+              [-lf*self.v1, 1+(lf-1)*(self.v1**2/V**2), (lf-1)*(self.v1*self.v2/V**2), (lf-1)*(self.v1*self.v3/V**2)],
+              [-lf*self.v2, (lf-1)*(self.v1*self.v2/V**2), 1+(lf-1)*(self.v2**2/V**2), (lf-1)*(self.v2*self.v3/V**2)],
+              [-lf*self.v3, (lf-1)*(self.v1*self.v3/V**2), (lf-1)*(self.v2*self.v3/V**2), 1+(lf-1)*(self.v3**2/V**2)]] 
+           
+        return Matrix(lB) * Matrix(vector) 
+         
+    def galileanT(self, vector, vector_field="spacetime"):
+        
+        gB = [[1, 0, 0, 0],
+              [self.v1, 1, 0, 0],
+              [self.v2, 0, 1, 0],
+              [self.v3, 0, 0, 1]] 
+        
+        return Matrix(gB) * Matrix(vector) 
 
 
+
+# define paramaters
+M, t, theta, r, phi= symbols(" M t theta r phi")
+metric = [[-(1-2*M/r),0,0,0], [0,1/(1-2*M/r),0,0], [0,0,r**2,0], [0,0,0,r**2*sin(theta)**2]]
+coor = [t,r,theta,phi]
+
+# initilaze manifold; coordinates order must match the metric's
 rm = RiemannManifold(coor, metric)
-gammas = rm.getChristofellSymbols(string=True)
-riemann_tensor = rm.getRiemannTensor(string=True)
-ricci_tensor = rm.getRicciTensor(string=True)
 
+# calculate christoffel symbols (Levi-Civita connection)
+christoffel_symbols = rm.getChristoffelSymbols(printstr=True)
+
+# calculate riemann tensor
+riemann_tensor = rm.getRiemannTensor(printstr=True)
+
+# stored solutions
+riemann_tensor_for_sphere = RiemannManifoldSolution().sphere(returned_tensor="riemann", printstr=True) 
